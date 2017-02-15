33eafd707d6a08db1b3ad5bd5b816f65e165e0fb
MinhND <dangminh@minhnd.com>
2017-02-15 13:49:34 +0700

    Add helper methods that returns intent of a Meaning object

--- messages.go
+++ messages.go
@@ -18,3 +18,16 @@ type Meaning struct {
 	Text string `json:"_text"`
 	Entities map[string][](map[string]interface{}) `json:"entities"`
 }
+
+// Intents is a helper method that returns intent fied of the meaning if have, 
+// otherwise returns empty string
+func (m *Meaning) Intent() string {
+	var intent string
+	if m.Entities["intent"] != nil {
+		t := m.Entities["intent"][0]["value"]
+		if str, ok := t.(string); ok {
+			intent = str
+		}
+	}
+	return intent
+}
