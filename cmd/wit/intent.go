6756e3e1473f4c0cd406716b9920f79124653adb
MinhND <dangminh@minhnd.com>
2017-03-21 11:08:29 +0700

    Move general parts to michlabs/nlu package

--- cmd/wit/intent.go
+++ cmd/wit/intent.go
@@ -1,22 +1,14 @@
 package main 
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
-	"log"
 
 	"github.com/michlabs/gowit"
+	"github.com/michlabs/nlu"
 )
 
-type IntentUtterance struct {
-	Intent string `json:"intent"`
-	Utterance string `json:"utterance"`
-}
-
 func TrainIntent(wit *gowit.Client, inputFP string) error {
-	ius, err := ReadIntentsFromFile(inputFP)
+	ius, err := nlu.ReadIntentsFromFile(inputFP)
 	if err != nil {
 		return err
 	}
@@ -39,7 +31,7 @@ func TrainIntent(wit *gowit.Client, inputFP string) error {
 }
 
 func TestIntent(wit *gowit.Client, inputFP string) error {
-	ius, err := ReadIntentsFromFile(inputFP)
+	ius, err := nlu.ReadIntentsFromFile(inputFP)
 	if err != nil {
 		return err
 	}
@@ -63,30 +55,4 @@ func TestIntent(wit *gowit.Client, inputFP string) error {
 	fmt.Printf("Result: %f\n", float64(correct_counter)/float64(total))
 
 	return nil
-}
-
-func ReadIntentsFromFile(inputFP string) ([]IntentUtterance, error) {
-	input, err := os.Open(inputFP)
-	if err != nil {
-		return nil, err
-	}
-	defer input.Close()
-
-	var ius []IntentUtterance
-	scanner := bufio.NewScanner(input)
-	counter := 0
-	for scanner.Scan() {
-		counter += 1
-		var iu IntentUtterance
-		text := strings.Replace(scanner.Text(), `"`, ``, -1) // Remove double quotes if have
-		tokens := strings.SplitN(text, ",", 2)
-		if len(tokens) < 2 {
-			log.Println("Skip line %d: it is not complete: %s", counter, scanner.Text())
-			continue
-		}
-		iu.Intent, iu.Utterance = strings.TrimSpace(tokens[0]), strings.TrimSpace(tokens[1])
-		ius = append(ius, iu)
-	}
-
-	return ius, nil
 }
